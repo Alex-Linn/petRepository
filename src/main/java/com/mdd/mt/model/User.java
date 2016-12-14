fdbeaf7f6b0871ac7a8ecbeec05a4c1ef9ef491d
wanglu_xu <wanglu_xu@suishouji.com>
2016-12-14 09:00:55 +0800

    pom.xml中ssm配置

---
+++ src/main/java/com/mdd/mt/model/User.java
@@ -0,0 +1,10 @@
+package com.mdd.mt.model;
+
+/**
+ * Created by Administrator on 2016/12/13.
+ */
+public class User {
+    public static void main(String[] args) {
+        System.out.println("");
+    }
+}
