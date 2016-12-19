19e646ecb9311fa0753c30f9cc475e9c5bcfc3bb
xwlmdd <1072552712@qq.com>
2016-12-19 23:37:26 +0800

    影片爬虫

--- src/main/java/com/mdd/mt/crawler/TaoBaoCrawler.java
+++ src/main/java/com/mdd/mt/crawler/TaoBaoCrawler.java
@@ -1,11 +1,13 @@
 package com.mdd.mt.crawler;
 
+import com.mdd.mt.model.Movie;
 import com.mdd.mt.model.StateCode;
 import org.apache.commons.codec.binary.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.jsoup.Jsoup;
 import org.jsoup.helper.StringUtil;
+import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import us.codecraft.webmagic.Page;
@@ -34,18 +36,63 @@ public class TaoBaoCrawler implements PageProcessor {
             Html indexHtml = page.getHtml();
             //正在热映电影
             String html = indexHtml.xpath("/html/body/div[4]/div[1]/div[2]/div[1]").get();
-            if(!StringUtil.isBlank(html)){
+            if (!StringUtil.isBlank(html)) {
                 Elements divElements = Jsoup.parse(html).select("div");
                 for (Element divElement : divElements) {
-                    String  movieUrl = divElement.select("a.movie-card-buy").attr("href");
+                    String movieUrl = divElement.select("a.movie-card-buy").attr("href");
                     //电影详细页面
                     page.addTargetRequest(movieUrl);
                 }
-            }else{
+            } else {
                 //电影名
-                String title = indexHtml.getDocument().select("body > div.detail-wrap.J_detailWrap > div.detail-cont > div > h3").text();
-                if(!StringUtil.isBlank(title)){
-                    //解析电影详细信息hu
+                String movieName = indexHtml.getDocument().select("body > div.detail-wrap.J_detailWrap > div.detail-cont > div > h3").text();
+                if (!StringUtil.isBlank(movieName)) {
+                    Document document = indexHtml.getDocument();
+                    Movie moive = new Movie();
+                    moive.setMovieName(movieName);
+                    //解析电影详细信息
+                    //英文名
+                    String movieEnglishName = document.select("body > div.detail-wrap.J_detailWrap > div.detail-cont > div > h3 > i").text();
+                    moive.setMovieEnglishName(movieEnglishName);
+                    //评分
+                    String score = document.select("body > div.detail-wrap.J_detailWrap > div.detail-cont > div > h3 > em").text();
+                    moive.setScore(Double.valueOf(score));
+                    //上映时间
+                    String rescheduledTime = document.select("body > div.detail-wrap.J_detailWrap > div.detail-cont > div > div.cont-time").text();
+                    moive.setRescheduledTime(rescheduledTime);
+                    //导演
+                    String director = document.select("body > div.detail-wrap.J_detailWrap > div.detail-cont > div > ul > li:nth-child(2)").text();
+                    moive.setDirector(director);
+                    //演员
+                    String performer = document.select("body > div.detail-wrap.J_detailWrap > div.detail-cont > div > ul > li:nth-child(3)").text();
+                    moive.setPerformer(performer);
+                    //影片类型
+                    String movieType = document.select("body > div.detail-wrap.J_detailWrap > div.detail-cont > div > ul > li:nth-child(4)").text();
+                    moive.setMovieType(movieType);
+                    //国家
+                    String country = document.select("body > div.detail-wrap.J_detailWrap > div.detail-cont > div > ul > li:nth-child(5)").text();
+                    moive.setCountry(country);
+                    //语言
+                    String language = document.select("body > div.detail-wrap.J_detailWrap > div.detail-cont > div > ul > li:nth-child(6)").text();
+                    moive.setLanguage(language);
+                    //时长
+                    String movieTime = document.select("body > div.detail-wrap.J_detailWrap > div.detail-cont > div > ul > li:nth-child(7)").text();
+                    moive.setMovieTime(movieTime);
+                    //影片简介
+                    String moiveStory = document.select("body > div.detail-wrap.J_detailWrap > div.detail-cont > div > ul > li.J_shrink.shrink").text();
+                    moive.setMoiveStory(moiveStory);
+                    //影片海报url
+                    String posterUrl = document.select("body > div.detail-wrap.J_detailWrap > div.detail-cont > div > div.cont-pic > img").attr("href");
+                    moive.setPosterUrl(posterUrl);
+                    //已经上映
+                    moive.setIsShow(1);
+                    //获取编号为163027城市的全部区域区域的影院信息
+                    //获取区域影院url参数
+                    String param = document.select("body > div.schedule-wrap.J_scheduleWrap.schedule-loaded > div.filter-wrap > div > ul > li:nth-child(1) > div > a.current").attr("data-param");
+                    //拼接获取所有区域的所有影院的url
+                    String cinemaUrl = "http://dianying.taobao.com/showDetailSchedule.htm?"+param;
+                    //影院详细页
+                    page.addTargetRequest(cinemaUrl);
 
                 }
             }
