c854e2995b8dd091be9a71e42880f1fc3f2d8f58
wanglu_xu <wanglu_xu@suishouji.com>
2017-01-10 18:39:59 +0800

    mybatis自动生成代码

---
+++ src/main/java/com/mdd/mt/mapper/CinemaMapper.java
@@ -0,0 +1,17 @@
+package com.mdd.mt.mapper;
+
+import com.mdd.mt.model.Cinema;
+
+public interface CinemaMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Cinema record);
+
+    int insertSelective(Cinema record);
+
+    Cinema selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Cinema record);
+
+    int updateByPrimaryKey(Cinema record);
+}
