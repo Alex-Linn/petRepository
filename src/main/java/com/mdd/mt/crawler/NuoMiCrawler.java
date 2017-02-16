17b4446fe876279ed3b40c7233a8efcf259354b8
xwlmdd <1072552712@qq.com>
2017-02-16 23:20:10 +0800

    百度糯米

--- src/main/java/com/mdd/mt/crawler/NuoMiCrawler.java
+++ src/main/java/com/mdd/mt/crawler/NuoMiCrawler.java
@@ -16,6 +16,10 @@ public class NuoMiCrawler {
 
     }
     public static void main(String[] args) throws IOException {
+ //获取所有的电影链接https://dianying.nuomi.com/common/ranklist?sortType=1&date=1487256419371
+//所有城市：https://dianying.nuomi.com/common/city/citylist?hasLetter=false&isjson=false
+// 影院：https://dianying.nuomi.com/movie/cinema?pagelets[]=pageletCinema&reqID=0&cityId=131&pageSize=6&movieId=62991
+
 //        https://dianying.nuomi.com/movie/movielist?cityId=340
 //        https://dianying.nuomi.com/movie/getmovielist?pagelets[]=pageletMovielist&reqID=0
 //        &type=hot&cityId=340&pageNum=0&pageSize=10
@@ -23,6 +27,8 @@ public class NuoMiCrawler {
 //        步骤：1、先拿到所有的电影的id
 //             2、通过id和城市名拼凑url ---->电影详细电影页
 //             https://dianying.nuomi.com/movie/detail?cityId=340&movieId=9966 电影详细页
-        System.out.println(Jsoup.connect("https://dianying.nuomi.com/movie/movielist?cityId=340").get());
+//        System.out.println(Jsoup.connect("https://dianying.nuomi.com/movie/movielist?cityId=340").get());
+
+        System.out.println(Jsoup.connect("https://dianying.nuomi.com/movie/detail?movieId=62991").get().toString());
     }
 }
