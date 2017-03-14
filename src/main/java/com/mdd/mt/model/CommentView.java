2c8abf741344023cca231508e8750348616ca80c
xwlmdd <1072552712@qq.com>
2017-03-15 00:37:24 +0800

    3.14

---
+++ src/main/java/com/mdd/mt/model/CommentView.java
@@ -0,0 +1,83 @@
+package com.mdd.mt.model;
+
+import java.util.Date;
+
+public class CommentView {
+	private Integer id;
+
+	private Integer movieId;
+
+	private Integer userId;
+	
+	private String userName;
+	
+	private String userPicture;
+
+	private Integer score;
+
+	private Integer dianzan;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Integer getMovieId() {
+		return movieId;
+	}
+
+	public void setMovieId(Integer movieId) {
+		this.movieId = movieId;
+	}
+
+	public Integer getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getUserPicture() {
+		return userPicture;
+	}
+
+	public void setUserPicture(String userPicture) {
+		this.userPicture = userPicture;
+	}
+
+	public Integer getScore() {
+		return score;
+	}
+
+	public void setScore(Integer score) {
+		this.score = score;
+	}
+
+	public Integer getDianzan() {
+		return dianzan;
+	}
+
+	public void setDianzan(Integer dianzan) {
+		this.dianzan = dianzan;
+	}
+
+	@Override
+	public String toString() {
+		return "CommentView [id=" + id + ", movieId=" + movieId + ", userId=" + userId + ", userName=" + userName
+				+ ", userPicture=" + userPicture + ", score=" + score + ", dianzan=" + dianzan + "]";
+	}
+
+	
+}
---
+++ src/main/java/com/mdd/mt/service/CommentServiceImpl.java
@@ -0,0 +1,26 @@
+package com.mdd.mt.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.mdd.mt.mapper.CommentMapper;
+import com.mdd.mt.model.Comment;
+
+@Component
+public class CommentServiceImpl {
+	
+	@Autowired
+	private CommentMapper commentMapper;
+	
+	/**
+	 * 获取电影的所有评论
+	 * @param movieId
+	 * @return
+	 */
+	public List<Comment>getCommentByMovieId(int movieId){
+		List<Comment> commentList = commentMapper.getCommentByMovieId(movieId);
+		return commentList;
+	}
+}
